3ba233fc183417d89e70b8d96e12cd0c0e7b3ed1
Heyy <fungnaomi@gmail.com>
2018-03-13 03:48:13 +0800

    -

---
+++ KidsPro - UI/Assets/Script/SwitchScenes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SwitchScenes : MonoBehaviour {
+
+
+    public void newScene(string name)
+    {
+        
+        SceneManager.LoadScene(name, LoadSceneMode.Additive);
+    }
+}
