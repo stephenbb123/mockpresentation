7dbe5012a13c2c888ce747d655e05d57a6ed7921
Heyy <fungnaomi@gmail.com>
2018-03-14 14:33:14 +0800

    -

---
+++ KidsPro - UI/Assets/RoadTool/Scripts/WebCamText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamText : MonoBehaviour {
+	
+	public GameObject webCameraPlane;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
