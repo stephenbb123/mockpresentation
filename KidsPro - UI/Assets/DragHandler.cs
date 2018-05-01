2bc1dfc2184988698d4a534ed0ec534713a0a3a6
mysing0311 <mysing0311@gmail.com>
2018-05-01 23:53:10 +0800

    dragdrop improved

---
+++ KidsPro - UI/Assets/DragHandler.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject item;
+	Vector3 startPosition;
+	Transform startParent;
+
+
+	public void OnBeginDrag (PointerEventData eventData){
+		item = gameObject;
+		startPosition = transform.position;
+		startParent = transform.parent;
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+
+	public void OnDrag (PointerEventData eventData){
+		transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag (PointerEventData eventData){
+		item = null;
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+		if(transform.parent == startParent){
+			transform.position = startPosition;
+		}
+	}
+}
