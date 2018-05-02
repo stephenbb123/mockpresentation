5c65663e094954a62caa8f90abbd154d96cead58
heeheyy <fungnaomi@gmail.com>
2018-05-02 23:49:50 +0800

    draft done

---
+++ KidsPro - UI/Assets/Script/Saved.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saved : MonoBehaviour
+{
+    
+
+    public void Save()
+    {
+        gameObject.SetActive(true);
+        new WaitForSecondsRealtime(1);
+        gameObject.SetActive(false);
+    }
+}
